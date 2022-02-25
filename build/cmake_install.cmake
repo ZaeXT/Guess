600cba5e5876160f1c926c6a7da1bff4e6ff4490
ZhanhCenter <zhanhxinlin@zhanh.onmicrosoft.com>
2022-02-25 22:52:57 +0800

    Elder Ver.

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Guess
+# Install script for directory: E:/Guess-Elder
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Guess/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/Guess-Elder/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
