79030d5a3f8efd6d52c65046cbe50feeb897e88c
Akaze <83901773+ZaeXT@users.noreply.github.com>
2023-06-22 20:12:23 +0800

    Reinitial The Project.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Guess
-This is a Project By ZaeXT.It is a simple C++ project.  
+# Guess(outdate-main)
+This is a Project By **ZaeXT**.It is a simple C++ project.  
 
 这是一个C++项目程序，由自学C++3天左右的人即兴创建，并不断更新（大概
 
-Main版本为基本版本，代码（“较为”）易懂，多采用函数实现相应功能，BUG修起来（应该）较为简单，功能更新较慢（因为不会...
+*Main*版本为基本版本，代码*较为*易懂，多采用函数实现相应功能，BUG修起来*应该*较为简单，功能更新较慢~（因为不会~...
+
+# Guess With Class(Main)
+学会**Class**用法后重新coding的***Guess***
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int T_Num();                //生成随机数
-void Start_Text();          //开始文本
-// void HelpMore();
-void WhileM(int num);       //主要功能实现函数
-void Fail();                //失败后选择函数
-// void if_FS();
-// void Chal_Time_S();
 int main()
 {
-    system("chcp 65001");   //设置命令行使用UTF-8标准   ---Windows用
-    system("cls");
-    int num = T_Num();      //获取随机数并赋值给 num 变量
-    // cout << num << endl;  //输出数字  ---Debug用
-    //玩家进行猜测
-    Start_Text();           //开始文本
-    string start = "aaa";   //初始化字符串start
-STARTF:                     //输入错误时再次调用
-    cin >> start;           //用户做出选择
-    //判断选择
-    if (start == "START")
-    {
-        cout << "开始挑战！" << endl;
-        system("cls");
-        WhileM(num);        //调用功能实现函数，传入随机数
-        cout << "挑战失败 输入 RESTART 重试或 END 退出" << endl;
-        Fail();             //调用失败后选择
-    }
-    else
-    {
-        cout << "输入错误 请重新输入！！！" << endl;
-        goto STARTF;        //游戏开始输入错误时使用
-    }
+    std::cout << "Initialzation The Project.\n";
+    return 0;
 }
